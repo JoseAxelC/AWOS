9afa1314200e75b40e7856d43c0a81477e1860f5
JoseAxelC <axelgarcia1091@gmail.com>
2026-03-19 11:54:48 -0600

    Update three.js

--- routes/three.js
+++ routes/three.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-// Servir archivos estáticos de tu proyecto Three.js
+// Proyecto Three
 router.use("/", express.static(path.join(__dirname, "../public/three")));
 
-// Servir librería three desde node_modules
-router.use("/lib", express.static(path.join(__dirname, "../node_modules/three")));
+// Librería Three
+router.use("/build", express.static(path.join(__dirname, "../node_modules/three/build")));
+router.use("/examples", express.static(path.join(__dirname, "../node_modules/three/examples")));
 
 module.exports = router;
