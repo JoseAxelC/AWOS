587aed93e83d20618ae416007297a377d275547a
JoseAxelC <axelgarcia1091@gmail.com>
2026-03-19 00:47:45 -0600

    Create tienda.js

---
+++ routes/tienda.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+
+router.post("/comprar", async (req, res) => {
+    // tu código igual
+});
+
+module.exports = router;
