b56be54c2351d31296862536237344c2f2390324
JoseAxelC <axelgarcia1091@gmail.com>
2026-03-19 17:36:35 -0600

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const HF_TOKEN = "TU_TOKEN";
+const HF_TOKEN = "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";
 
 router.post("/", async (req, res) => {
 
