828d0071fd3454a631eff866f1b553f872746054
JoseAxelC <axelgarcia1091@gmail.com>
2026-03-19 13:51:49 -0600

    Update auth2fa.js

--- routes/auth2fa.js
+++ routes/auth2fa.js
@@ -1,10 +1,46 @@
 const express = require("express");
-const router = express.Router();
+const speakeasy = require("speakeasy");
+const QRCode = require("qrcode");
 
-// tus rutas aquí
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-router.get("/", (req, res) => {
-    res.send("2FA funcionando al 100");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let userSecret = null;
+
+// 🔐 Generar QR
+app.get("/auth/generar", async (req, res) => {
+
+  const secret = speakeasy.generateSecret({
+    length: 20,
+    name: "Proyecto2FA(Axel)"
+  });
+
+  userSecret = secret.base32;
+
+  const qr = await QRCode.toDataURL(secret.otpauth_url);
+
+  res.json({ qr }); // 👈 ahora responde JSON
 });
 
-module.exports = router;
+// 🔐 Verificar código
+app.post("/auth/verificar", (req, res) => {
+
+  const { token } = req.body;
+
+  const verified = speakeasy.totp.verify({
+    secret: userSecret,
+    encoding: "base32",
+    token: token,
+    window: 1
+  });
+
+  res.json({ success: verified });
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
+});
