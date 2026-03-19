c47da7d5df9c96177112d70772c594a69badc0c8
JoseAxelC <axelgarcia1091@gmail.com>
2026-03-19 17:46:31 -0600

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -3,36 +3,24 @@ const axios = require("axios");
 
 const router = express.Router();
 
-const HF_TOKEN = process.env.HF_TOKEN;
-
 router.post("/", async (req, res) => {
 
     const mensaje = req.body.texto;
 
-    if (!mensaje.toLowerCase().includes("java")) {
-        return res.json({ respuesta: "Solo puedo hablar sobre programación en Java." });
-    }
-
     try {
-
         const response = await axios.post(
-            "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2",
+            "https://api-inference.huggingface.co/models/google/flan-t5-base",
             {
                 inputs: mensaje
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${HF_TOKEN}`
-                }
             }
         );
 
-        const respuestaIA = response.data[0]?.generated_text || "No hubo respuesta";
+        const respuesta = response.data[0]?.generated_text || "Sin respuesta";
 
-        res.json({ respuesta: respuestaIA });
+        res.json({ respuesta });
 
     } catch (error) {
-        console.error("ERROR IA:", error.response?.data || error.message);
+        console.error("ERROR REAL:", error.response?.data || error.message);
         res.json({ respuesta: "Error IA" });
     }
 });
